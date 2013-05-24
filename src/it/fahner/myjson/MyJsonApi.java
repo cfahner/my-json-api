3d5e5d88053caf4395d1a2949f7045a691e78002
C. Fahner <christiaanenzo@gmail.com>
2013-05-25 01:48:52 +0200

    implement MyJsonApiListener service update documentation

--- src/it/fahner/myjson/MyJsonApi.java
+++ src/it/fahner/myjson/MyJsonApi.java
@@ -12,7 +12,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
- */
+*/
 
 package it.fahner.myjson;
 
@@ -22,18 +22,18 @@ import java.util.HashMap;
 /**
  * The main entry point for the MyJsonApi library. All requests, threads, caches and listeners
  * are managed here.
- * You must ALWAYS configure this class before using it. Example:
- * <code>
+ * <p>You must ALWAYS configure MyJsonApi before using it. For example:</p>
+ * <p><code>
  * MyJsonApi.setConfigs(new MyJsonConfigs("http://hostname.com/api/location/"));
- * </code>
- * @since 1.0
+ * </code></p>
+ * @since MyJsonApi 1.0
  * @author C. Fahner <info@fahnerit.com>
  */
 public final class MyJsonApi {
 	
 	/**
 	 * Represents the current version of this library.
-	 * @since 1.0
+	 * @since MyJsonApi 1.0
 	 */
 	public static final String VERSION = "1.0";
 	
@@ -45,24 +45,50 @@ public final class MyJsonApi {
 	
 	/**
 	 * Initializes the MyJsonApi. It is possible to pass new configurations later.
-	 * @since 1.0
+	 * @since MyJsonApi 1.0
 	 * @param configs Configurations to use from now on
 	 */
 	public static void initialize(MyJsonConfigs configs) {
 		MyJsonApi.configs = configs;
 	}
 	
+	/**
+	 * Registers a listener to MyJsonApi. The listener will (from now on) receive updates
+	 * from MyJsonApi when MyRequests sent by that listener are resolved.
+	 * @since MyJsonApi 1.0
+	 * @param listener The listener to register
+	 */
 	public static void startListening(MyJsonApiListener listener) {
-		
+		forceInit();
+		String cls = listener.getClass().getName();
+		ArrayList<MyJsonApiListener> activeListeners = listeners.get(cls);
+		if (activeListeners == null) { activeListeners = new ArrayList<MyJsonApiListener>(); }
+		activeListeners.add(listener);
+		listeners.put(cls, activeListeners);
 	}
 	
+	/**
+	 * Stops a listener from receiving any updates about the MyRequests it sends.
+	 * @since MyJsonApi 1.0
+	 * @param listener The listener that should no longer receive updates
+	 */
 	public static void stopListening(MyJsonApiListener listener) {
-		
+		forceInit();
+		String cls = listener.getClass().getName();
+		ArrayList<MyJsonApiListener> activeListeners = listeners.get(cls);
+		if (activeListeners == null) { return; } // no listeners anyway
+		activeListeners.remove(listener);
+		if (activeListeners.size() > 0) { listeners.put(cls, activeListeners); }
+		else { listeners.remove(cls); }
 	}
 	
 	private static MyJsonConfigs getConfigs() {
-		if (configs == null) { throw new RuntimeException("MyJsonApi not initialized!"); }
+		forceInit();
 		return configs;
+	}
+	
+	private static void forceInit() {
+		if (configs == null) { throw new RuntimeException("MyJsonApi not initialized!"); }
 	}
 	
 	private static String getLocation() {
