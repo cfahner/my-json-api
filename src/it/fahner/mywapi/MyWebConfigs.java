a474b538696b0557b22cace9dc346bf670e67db1
C. Fahner <christiaanenzo@gmail.com>
2013-05-27 00:50:09 +0200

    more configurations renamed some methods

--- src/it/fahner/mywapi/MyWebConfigs.java
+++ src/it/fahner/mywapi/MyWebConfigs.java
@@ -16,6 +16,8 @@
 
 package it.fahner.mywapi;
 
+import it.fahner.mywapi.http.HttpParamList;
+
 /**
  * A configuration object for MyWebApi. Contains at least the URL that points to the Api.
  * @since MyWebApi 1.0
@@ -23,16 +25,34 @@ package it.fahner.mywapi;
  */
 public class MyWebConfigs {
 	
+	/**
+	 * The default request timeout in milliseconds to use when none are specified.
+	 * @since MyWebApi 1.0
+	 */
+	public static final int DEFAULT_TIMEOUT = 15000;
+	
 	/** Stores the URL that points to the Api. */
-	private String location;
+	private String baseUrl;
+	
+	/** Stores the time to wait before a request should timeout (in milliseconds). */
+	private int requestTimeout;
+	
+	/** Stores the parameters that always will be appended to the URL. */
+	private HttpParamList persistentUrlParams;
+	
+	/** Flag indication wether or not to use caches. */
+	private boolean useCache;
 	
 	/**
 	 * Creates a new Api configurations object.
 	 * @since MyWebApi 1.0
-	 * @param location The URL that points to the Api
+	 * @param baseUrl The base URL that points to the Api
 	 */
-	public MyWebConfigs(String location) {
-		this.location = location;
+	public MyWebConfigs(String baseUrl) {
+		this.baseUrl = baseUrl;
+		this.requestTimeout = DEFAULT_TIMEOUT;
+		this.persistentUrlParams = new HttpParamList();
+		this.useCache = false;
 	}
 	
 	/**
@@ -40,8 +60,69 @@ public class MyWebConfigs {
 	 * @since MyWebApi 1.0
 	 * @return The Api URL
 	 */
-	public String getLocation() {
-		return location;
+	public String getBaseUrl() {
+		return baseUrl;
+	}
+	
+	/**
+	 * Returns the currently set request timeout in milliseconds. This is set to
+	 * DEFAULT_TIMEOUT by default.
+	 * @since MyWebApi 1.0
+	 * @return The timeout in milliseconds
+	 */
+	public int getTimeout() {
+		return this.requestTimeout;
+	}
+	
+	/**
+	 * Specifies the request timeout in milliseconds.
+	 * @since MyWebApi 1.0
+	 * @param requestTimeout The new timeout in milliseconds
+	 * @return This MyWebConfigs for call chaining
+	 */
+	public MyWebConfigs setTimeout(int requestTimeout) {
+		this.requestTimeout = requestTimeout;
+		return this;
+	}
+	
+	/**
+	 * Returns the HttpParamList that contains all persistent URL parameters.
+	 * @since MyWebApi 1.0
+	 * @return This MyWebConfigs for call chaining
+	 */
+	public HttpParamList getPersistentUrlParams() {
+		return this.persistentUrlParams;
+	}
+	
+	/**
+	 * Sets the parameter list that contains all persistent URL parameters.
+	 * @since MyWebApi 1.0
+	 * @param params The new parameter list to use
+	 * @return This MyWebConfigs for call chaining
+	 */
+	public MyWebConfigs setPersistentUrlParams(HttpParamList params) {
+		this.persistentUrlParams = params;
+		return this;
+	}
+	
+	/**
+	 * Returns the flag that indicates cache usage.
+	 * @since MyWebApi 1.0
+	 * @return TRUE when caches can be used, FALSE otherwise
+	 */
+	public boolean useCache() {
+		return this.useCache;
+	}
+	
+	/**
+	 * Sets the flag indication wether or not to use caches.
+	 * @since MyWebApi 1.0
+	 * @param useCache TRUE to use caches, FALSE otherwise
+	 * @return This MyWebConfigs for call chaining
+	 */
+	public MyWebConfigs useCache(boolean useCache) {
+		this.useCache = useCache;
+		return this;
 	}
 	
 }
