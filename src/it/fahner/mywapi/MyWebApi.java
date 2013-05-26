80763d4286bcc480a04ee9f0e0055ad093c510d5
C. Fahner <christiaanenzo@gmail.com>
2013-05-27 00:50:15 +0200

    update MyWebApi

--- src/it/fahner/mywapi/MyWebApi.java
+++ src/it/fahner/mywapi/MyWebApi.java
@@ -16,21 +16,25 @@
 
 package it.fahner.mywapi;
 
+import it.fahner.mywapi.http.HttpRequestThread;
+
 import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * The main entry point for the MyWebApi library. All requests, threads, caches and listeners
  * are managed here.
- * <p>You must ALWAYS configure MyWebApi before using it. For example:</p>
+ * <p>Most basic instantiation:</p>
  * <p><code>
- * MyWebApi.initialize(new MyWebConfigs("http://hostname.com/api/location/"));
+ * MyWebApi apiInstance = new MyWebApi(new MyWebConfigs("http://hostname.com/api/location/"));
  * </code></p>
  * @since MyWebApi 1.0
  * @author C. Fahner <info@fahnerit.com>
  */
-public final class MyWebApi {
+public class MyWebApi {
 	
 	/**
 	 * Represents the current version of this library.
@@ -39,20 +43,32 @@ public final class MyWebApi {
 	public static final String VERSION = "1.0";
 	
 	/** Contains the initialization information. */
-	private static MyWebConfigs configs;
+	private MyWebConfigs configs;
 	
-	/** Stores all things that listen to this Api. */
-	private static HashMap<String, ArrayList<WeakReference<MyWebApiListener>>> listeners;
+	/** Stores all things that listen to this API. */
+	private ArrayList<WeakReference<MyWebApiListener>> listeners;
+	
+	/** Stores the unique HttpRequest identifiers for all currently open requests. */
+	private HashSet<String> requests;
 	
 	/**
-	 * Initializes the MyWebApi. You can only call this method once. Use the setter methods provided by
-	 * MyWebApi to change some settings later.
-	 * @since MyWebApi 1.0
-	 * @param configs Configurations to use from now on
+	 * Creates a new access point to a web-based API.
 	 */
-	public static void initialize(MyWebConfigs configs) {
-		MyWebApi.configs = configs;
-		listeners = new HashMap<String, ArrayList<WeakReference<MyWebApiListener>>>();
+	public MyWebApi(MyWebConfigs configs) {
+		this.configs = configs;
+		listeners = new ArrayList<WeakReference<MyWebApiListener>>();
+	}
+	
+	public HttpRequestThread convertToHttpRequest(MyRequest myReq) {
+		String urlToUse = getBaseURL();
+		try {
+			URL url = new URL(getBaseURL() + myReq.getPath());
+		} catch (MalformedURLException e) {
+			System.err.println("MyWebApi: malformed URL, reverting to base URL");
+		}
+		HttpRequestThread out = new HttpRequestThread(urlToUse);
+		
+		return out;
 	}
 	
 	/**
@@ -61,27 +77,29 @@ public final class MyWebApi {
 	 * @since MyWebApi 1.0
 	 * @param listener The listener to register
 	 */
-	public static void startListening(MyWebApiListener listener) {
-		forceInit();
-		String cls = listener.getClass().getName();
-		ArrayList<WeakReference<MyWebApiListener>> activeListeners = listeners.get(cls);
-		if (activeListeners == null) { activeListeners = new ArrayList<WeakReference<MyWebApiListener>>(); }
-		activeListeners.add(new WeakReference<MyWebApiListener>(listener));
-		listeners.put(cls, activeListeners);
+	public void startListening(MyWebApiListener listener) {
+		listeners.add(new WeakReference<MyWebApiListener>(listener));
+	}
+	
+	/**
+	 * Starts a single request. Invokes the callbacks for all listeners when the request
+	 * has been resolved.
+	 * <p>If MyWebApi is still waiting for another request that points to the same resource (to the same URL
+	 * with the same parameters), no new request will be started.</p>
+	 * <p>If an response is stored in the cache and has not yet expired, that response is returned
+	 * instead of sending a new request.</p>
+	 * @param request An implementation of MyRequest that needs to be resolved
+	 */
+	public void startRequest(MyRequest request) {
+		
 	}
 	
 	/**
 	 * Shorthand getter for getting the Api location.
 	 * @return The URL that points to the API
 	 */
-	private static String getBaseURL() {
-		forceInit();
-		return configs.getLocation();
-	}
-	
-	/** Forces an initialization to have happened. */
-	private static void forceInit() {
-		if (configs == null) { throw new RuntimeException("MyWebApi not initialized!"); }
+	private String getBaseURL() {
+		return configs.getBaseUrl();
 	}
 	
 }
