f743f59a8882f8e8160996407d7330de7696470a
C. Fahner <christiaanenzo@gmail.com>
2013-05-26 19:45:55 +0200

    rename MyHttpWorker to HttpWorker separated HttpRequestMethod from HttpWorker class

---
+++ src/it/fahner/mywapi/http/HttpRequestMethod.java
@@ -0,0 +1,47 @@
+/*
+ Copyright 2013 FahnerIT
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package it.fahner.mywapi.http;
+
+/**
+ * An HTTP request method supported by HttpWorker.
+ * @since MyWebApi 1.0
+ * @author C. Fahner <info@fahnerit.com>
+ */
+public enum HttpRequestMethod {
+	
+	/** HTTP's GET request method. */
+	GET,
+	
+	/** HTTP's POST request method. */
+	POST,
+	
+	/** HTTP's HEAD request method. */
+	HEAD,
+	
+	/** HTTP's OPTIONS request method. */
+	OPTIONS,
+	
+	/** HTTP's PUT request method. */
+	PUT,
+	
+	/** HTTP's DELETE request method. */
+	DELETE,
+	
+	/** HTTP's TRACE request method. Some platforms don't allow this method. */
+	TRACE
+	
+}
--- src/it/fahner/mywapi/http/HttpWorker.java
+++ src/it/fahner/mywapi/http/HttpWorker.java
@@ -14,7 +14,7 @@
    limitations under the License.
 */
 
-package it.fahner.mywapi;
+package it.fahner.mywapi.http;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -30,7 +30,7 @@ import java.util.Locale;
  * <p>
  * Usage:<br>
  * <code>
- * MyHttpWorker myRun = new MyHttpWorker("http://api/url")<br>
+ * HttpWorker myRun = new HttpWorker("http://api/url")<br>
  * 		.setRequestBodyContent("content=value")<br>
  * 		.setTimeout(15000) // 15 seconds timeout<br>
  * 		.setListener(this); // supply a listener<br>
@@ -40,7 +40,7 @@ import java.util.Locale;
  * @since MyWebApi 1.0
  * @author C. Fahner <info@fahnerit.com>
  */
-public class MyHttpWorker extends Thread {
+public class HttpWorker extends Thread {
 	
 	/**
 	 * The default encoding that is assumed when sending and receiving HTTP documents.
@@ -58,43 +58,13 @@ public class MyHttpWorker extends Thread {
 	 * The default request method used.
 	 * @since MyWebApi 1.0
 	 */
-	public static final RequestMethod DEFAULT_REQUEST_METHOD = RequestMethod.GET;
+	public static final HttpRequestMethod DEFAULT_REQUEST_METHOD = HttpRequestMethod.GET;
 	
 	/**
 	 * The default timeout in milliseconds of an HTTP request.
 	 * @since MyWebApi 1.0
 	 */
 	public static final int DEFAULT_TIMEOUT = 15000;
-	
-	/**
-	 * An HTTP request method supported by MyHttpWorker.
-	 * @since MyWebApi 1.0
-	 * @author C. Fahner <info@fahnerit.com>
-	 */
-	public static enum RequestMethod {
-		
-		/** HTTP's GET request method. */
-		GET,
-		
-		/** HTTP's POST request method. */
-		POST,
-		
-		/** HTTP's HEAD request method. */
-		HEAD,
-		
-		/** HTTP's OPTIONS request method. */
-		OPTIONS,
-		
-		/** HTTP's PUT request method. */
-		PUT,
-		
-		/** HTTP's DELETE request method. */
-		DELETE,
-		
-		/** HTTP's TRACE request method. Some platforms don't allow this method. */
-		TRACE
-		
-	}
 	
 	/** The connection object that has been constructed. */
 	HttpURLConnection connection;
@@ -106,7 +76,7 @@ public class MyHttpWorker extends Thread {
 	private String contentType;
 	
 	/** Stores the current request method. */
-	private RequestMethod requestMethod;
+	private HttpRequestMethod requestMethod;
 	
 	/** Contains the contents to put in the request body. */
 	private String requestBodyContent;
@@ -114,17 +84,17 @@ public class MyHttpWorker extends Thread {
 	/** Contains the timeout in milliseconds after which this request is aborted. */
 	private int timeoutMillis;
 	
-	/** Stores the callbacks to invoke when this MyHttpWorker completes its work. */
-	private MyHttpWorkerListener listener;
+	/** Stores the callbacks to invoke when this HttpWorker completes its work. */
+	private HttpWorkerListener listener;
 	
 	/**
-	 * Creates a new MyHttpWorker, but does not yet start any networking I/O. Remember to set a
-	 * MyHttpWorkerListener if you want to be informed about the results. Not specifying a listener
-	 * means this MyHttpWorker will just fire and forget.
+	 * Creates a new HttpWorker, but does not yet start any networking I/O. Remember to set a
+	 * HttpWorkerListener if you want to be informed about the results. Not specifying a listener
+	 * means this HttpWorker will just fire and forget.
 	 * @since MyWebApi 1.0
 	 * @param url The URL to connect to, including the UrlParameters
 	 */
-	public MyHttpWorker(String url) {
+	public HttpWorker(String url) {
 		encoding = DEFAULT_ENCODING;
 		contentType = DEFAULT_CONTENT_TYPE;
 		requestMethod = DEFAULT_REQUEST_METHOD;
@@ -139,7 +109,6 @@ public class MyHttpWorker extends Thread {
 			connection.setUseCaches(false);
 		} catch (Exception e) {
 			// Probably a malformed url exception, no exceptions should occur here anyways so crash the application
-			MyLog.warn("Could not create HTTP worker thread!");
 			throw new RuntimeException(e.getMessage());
 		}
 	}
@@ -150,9 +119,9 @@ public class MyHttpWorker extends Thread {
 	 * If the response contains content-encoding information, this will be used to decode the response.
 	 * @since MyWebApi 1.0
 	 * @param encoding The new encoding to set
-	 * @return This MyHttpWorker for call chaining
+	 * @return This HttpWorker for call chaining
 	 */
-	public MyHttpWorker setEncoding(String encoding) {
+	public HttpWorker setEncoding(String encoding) {
 		this.encoding = encoding;
 		return this;
 	}
@@ -161,10 +130,10 @@ public class MyHttpWorker extends Thread {
 	 * Changes the content type of the requesting document. Default is DEFAULT_CONTENT_TYPE.
 	 * Modifies the Content-Type HTTP header of the request.
 	 * @since MyWebApi 1.0
-	 * @param contentType The new content type to use
-	 * @return This MyHttpWorker for call chaining.
+	 * @param responseContentType The new content type to use
+	 * @return This HttpWorker for call chaining.
 	 */
-	public MyHttpWorker setContentType(String contentType) {
+	public HttpWorker setContentType(String contentType) {
 		this.contentType = contentType;
 		return this;
 	}
@@ -173,21 +142,21 @@ public class MyHttpWorker extends Thread {
 	 * Specifies the request method of the HTTP request. The default method is GET.
 	 * @since MyWebApi 1.0
 	 * @param requestMethod The new request method to change to
-	 * @return This MyHttpWorker for call chaining
+	 * @return This HttpWorker for call chaining
 	 */
-	public MyHttpWorker setRequestMethod(RequestMethod requestMethod) {
+	public HttpWorker setRequestMethod(HttpRequestMethod requestMethod) {
 		this.requestMethod = requestMethod;
 		return this;
 	}
 	
 	/**
-	 * Changes the connection timeout of this MyHttpWorker.
+	 * Changes the connection timeout of this HttpWorker.
 	 * <p>Note: some platforms do not allow changing the timeout.</p>
 	 * @since MyWebApy 1.0
 	 * @param timeoutMillis The time in milliseconds before timing out
-	 * @return This MyHttpWorker for call chaining
+	 * @return This HttpWorker for call chaining
 	 */
-	public MyHttpWorker setTimeout(int timeoutMillis) {
+	public HttpWorker setTimeout(int timeoutMillis) {
 		this.timeoutMillis = timeoutMillis;
 		return this;
 	}
@@ -196,20 +165,20 @@ public class MyHttpWorker extends Thread {
 	 * Sets the request body to the given value.
 	 * @since MyWebApi 1.0
 	 * @param requestBodyContent The new content to set the request body to
-	 * @return This MyHttpWorker for call chaining
+	 * @return This HttpWorker for call chaining
 	 */
-	public MyHttpWorker setRequestBody(String requestBodyContent) {
+	public HttpWorker setRequestBody(String requestBodyContent) {
 		this.requestBodyContent = requestBodyContent;
 		return this;
 	}
 	
 	/**
-	 * Registers a set of callbacks to be invoked when this MyHttpWorker completes its tasks.
+	 * Registers a set of callbacks to be invoked when this HttpWorker completes its tasks.
 	 * @since MyWebApi 1.0
 	 * @param listener The callbacks to register
-	 * @return This MyHttpWorker for call chaining
+	 * @return This HttpWorker for call chaining
 	 */
-	public MyHttpWorker setMyHttpWorkerListener(MyHttpWorkerListener listener) {
+	public HttpWorker setMyHttpWorkerListener(HttpWorkerListener listener) {
 		this.listener = listener;
 		return this;
 	}
@@ -247,9 +216,9 @@ public class MyHttpWorker extends Thread {
 			// Create a response object and call the callbacks only when a listener has been specified
 			if (listener == null) { return; }
 			Response response = new Response();
-			if (connection.getContentType() != null) { response.contentType = connection.getContentType(); }
-			response.contentEncoding = responseEnctype;
-			response.contentBody = responseBody.toString();
+			if (connection.getContentType() != null) { response.responseContentType = connection.getContentType(); }
+			response.responseEncoding = responseEnctype;
+			response.responseBody = responseBody.toString();
 			response.httpStatusCode = connection.getResponseCode();
 			listener.onWorkerFinished(response);
 		} catch (IOException e) {
@@ -260,12 +229,12 @@ public class MyHttpWorker extends Thread {
 	
 	@Override
 	public String toString() {
-		return "{MyHttpWorker.Request => '" + connection.getURL().toString() + "'}";
+		return "{HttpWorker.Request => '" + connection.getURL().toString() + "'}";
 	}
 	
 	/**
-	 * Defines a MyHttpWorker response. An instance of this class is returned when an HTTP request
-	 * has been resolved by this MyHttpWorker. Only exposes the information meaningful to MyWebApi.
+	 * Defines a HttpWorker response. An instance of this class is returned when an HTTP request
+	 * has been resolved by this HttpWorker. Only exposes the information meaningful to MyWebApi.
 	 * @since MyWebApi 1.0
 	 * @author C. Fahner <info@fahnerit.com>
 	 */
@@ -275,26 +244,32 @@ public class MyHttpWorker extends Thread {
 		 * Contains the name of the remote resource that this response contains (a URL).
 		 * @since MyWebApi 1.0
 		 */
-		public String remoteResource = connection.getURL().toExternalForm();
+		public String requestUrl = connection.getURL().toExternalForm();
+		
+		/**
+		 * Contains the request method used to retrieve this response.
+		 * @since MyWebApi 1.0
+		 */
+		public HttpRequestMethod requestMethod = HttpWorker.this.requestMethod;
 		
 		/**
 		 * The content type of the response. Assumes "text/plain" when not specified by the response.
 		 * @since MyWebApi 1.0
 		 */
-		public String contentType = "text/plain";
+		public String responseContentType = "text/plain";
 		
 		/**
 		 * The content encoding of the response. Assumes the content encoding of the original request when not
 		 * specified by the response.
 		 * @since MyWebApi 1.0
 		 */
-		public String contentEncoding = encoding;
+		public String responseEncoding = encoding;
 		
 		/**
 		 * The content body of the response. Defaults to the empty string.
 		 * @since MyWebApi 1.0
 		 */
-		public String contentBody = "";
+		public String responseBody = "";
 		
 		/**
 		 * The HTTP status code of the response. Defaults to 204 (No Content).
@@ -304,17 +279,17 @@ public class MyHttpWorker extends Thread {
 		
 		@Override
 		public String toString() {
-			return "{MyHttpWorker.Response <= '" + remoteResource + "}";
+			return "{HttpWorker.Response <= '" + requestUrl + "}";
 		}
 		
 	}
 	
 	/**
-	 * The callbacks to be invoked when a MyHttpWorker has completed it's operations.
+	 * The callbacks to be invoked when a HttpWorker has completed it's operations.
 	 * @since MyWebApi 1.0
 	 * @author C. Fahner <info@fahnerit.com>
 	 */
-	public static interface MyHttpWorkerListener {
+	public static interface HttpWorkerListener {
 		
 		/**
 		 * Called when the work has successfully received a response. Note that this also includes
